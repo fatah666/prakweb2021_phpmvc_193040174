ea17447710db2db33e0e72523da8ae2a019358d8
ujangfatahak <63782886+ujangfatahak@users.noreply.github.com>
2021-10-01 15:08:41 +0700

    menyelesaikan video ke 5 mengenai komponen view

---
+++ app/views/about/index.php
@@ -0,0 +1,2 @@
+<h1>About Me</h1>
+<p>Halo, nama saya <?= $data['nama']; ?>, saya adalah <?= $data['pekerjaan']; ?>, umur saya <?= $data['umur']; ?> tahun </p>
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Ujang Fatah Abwaba Khoer
+193040174
+https://github.com/fatah666/prakweb2021_oophp_193040174.git
+*/
+require_once '../app/init.php';
+
+$app = new App;
