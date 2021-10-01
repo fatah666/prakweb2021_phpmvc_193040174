cf0fa9319735067182c1f5f203f60b009ce226eb
ujangfatahak <63782886+ujangfatahak@users.noreply.github.com>
2021-10-01 15:53:15 +0700

    menyelesaikan video 6 mengenai assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>About Me</h1>
-<p>Halo, nama saya <?= $data['nama']; ?>, saya adalah <?= $data['pekerjaan']; ?>, umur saya <?= $data['umur']; ?> tahun </p>
+<div class="container">
+  <h1 class="mt-4">About Me</h1>
+  <img src="<?= BASEURL; ?>/img/profile.jpg" alt="Fatah" width="200" class="rounded-circle shadow">
+  <p>Halo, nama saya <?= $data['nama']; ?>, saya adalah <?= $data['pekerjaan']; ?>, umur saya <?= $data['umur']; ?> tahun </p>
+</div>
